e9de10063b91e24b2cd2b56dc1442eb7e1df1214
badermc123 <43385932+badermc123@users.noreply.github.com>
2018-09-23 14:25:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,18 +3,12 @@ const client = new Discord.Client();
 let timer;
 console.log("BOT ONLINE");
 
-client.on('message', message => {
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`سباام يولد يلعن اومم الفله ${x}`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
+  client.on('ready', async () => {
+      let ReBeL = ["Hii","Lol","wine","bader","Kiiiiiiiing","xD"]
+  setInterval(() => {
+client.channels.get("491690814951522305").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
+},60000);
 });
+  
 
 client.login(process.env.BOT_TOKEN);
