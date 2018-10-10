2ea487f6db00f2d777d7229e01f0e13a46244466
badermc123 <43385932+badermc123@users.noreply.github.com>
2018-10-10 23:14:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,40 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
+
 let timer;
+
 console.log("BOT ONLINE");
 
   client.on('ready', async () => {
-      let ReBeL = ["Hii","Lol","wine","bader","Kiiiiiiiing","xD"]
+
+      let ReBeL = ["Hiiiiiiiiiii","Looooooool","frieends","badeeeeer","Kiiiiiiiing","xDDDDDDDD"]
+
   setInterval(() => {
+
 client.channels.get("498855781232279563").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
+
 },10000);
+
 });
   
+
+client.on('ready', () => {
+
+    let channel_id = "491586185605545986";
+
+    var channel = client.channels.get(channel_id);
+
+    if(channel.type == 'voice') {
+
+        channel.join();
+
+    } else {
+
+        console.log('...');
+
+    }
+
+});
 
 client.login(process.env.BOT_TOKEN);
