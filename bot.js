742ce9c194ec2a7f020fbbf1cf0a2fd4c8c0a7ae
6n6l <43895200+6n6l@users.noreply.github.com>
2019-11-03 15:11:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,15 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
-const myid = ['476130473681092608'];// ايدي حسابك
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+const myid = ['462752734140039168'];// ايدي حسابك
 const prefix = ['!'];
  
 client.on('ready', () => {
    console.log(`----------------`);
-   console.log(`Credit Farmm - Script By : Sir_Bader. `);
+   console.log(`Credit Farmm - Script By : TheBeatz `);
    console.log(`----------------`);
    console.log(`Loadinng`);
    console.log(`Loadinng.`);
@@ -16,13 +19,31 @@ client.on('ready', () => {
    console.log(`----------------`);
 });
  
+ 
 client2.on('ready', () => {
-   console.log(`----------------`);
-   console.log(`2 Account Online ✩' `);
-   console.log(`----------------`);
-});
- 
- 
+    console.log(`----------------`);
+    console.log(`2 Account Online ✩' `);
+    console.log(`----------------`);
+ });
+
+ client3.on('ready', () => {
+    console.log(`----------------`);
+    console.log(`3 Account Online ✩' `);
+    console.log(`----------------`);
+ });
+
+ client4.on('ready', () => {
+    console.log(`----------------`);
+    console.log(`4 Account Online ✩' `);
+    console.log(`----------------`);
+ });
+
+ client5.on('ready', () => {
+    console.log(`----------------`);
+    console.log(`5 Account Online ✩' `);
+    console.log(`----------------`);
+ }); 
+
  
 client.on('message', message => {
     if(message.content === prefix+'راتب'){
@@ -46,8 +67,8 @@ client.on('message', message => {
 if (message.content === prefix+'spam') {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**NEW spam code Sir_Bader.  - الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر ر الله اكبر الله اكبر **[ " ${x} " ]`)
+      for(let x = 0; x < 9984511; x++) {
+        message.channel.send(`**NEW spam code TheBeatz  - الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر ر الله اكبر الله اكبر **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -68,7 +89,7 @@ client.on('message', message => {
  
  
 if (command == "تحدث") {
-let rank = message.guild.member(message.author).roles.find('Sir_Bader.', 'Role.n3k4a'); //لازم تعمل الرتبه دي في السيرفر  Role.n3k4a
+let rank = message.guild.member(message.author).roles.find('TheBeatz', 'admin'); //لازم تعمل الرتبه دي في السيرفر  admin
 if (!rank) return message.reply(' ')
   message.channel.send(args.join("  "))
     message.delete();
@@ -97,8 +118,8 @@ client2.on('message', message => {
 if (message.content === prefix+'spam') {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**NEW spam code by Sir_Bader.  - الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر ر الله اكبر الله اكبر **[ " ${x} " ]`)
+      for(let x = 0; x < 9984511; x++) {
+        message.channel.send(`**NEW spam code by TheBeatz  - الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر ر الله اكبر الله اكبر **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -119,13 +140,177 @@ client2.on('message', message => {
  
  
 if (command == "تحدث") {
-let rank = message.guild.member(message.author).roles.find('Sir_Bader.', 'Role.n3k4a');
+let rank = message.guild.member(message.author).roles.find('TheBeatz', 'admin');
 if (!rank) return message.reply(' ')
   message.channel.send(args.join("  "))
     message.delete();
   }
 });
  
+
+
+//client3
+
+
+client3.on('message', message => {
+    if(message.content === prefix+'راتب'){
+        message.channel.send('#daily')
+    }
+});
+ 
+client3.on('message', message => {
+    if(message.content === prefix+'مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+ 
+client3.on('message', message => {
+    if(message.content === prefix+'ريب'){
+        message.channel.send("#rep "+"<@" + myid + ">")
+    }
+});
+ 
+client3.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 9984511; x++) {
+        message.channel.send(`**NEW spam code by TheBeatz  - الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر ر الله اكبر الله اكبر **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+         
+        }
+      }
+});
+ 
+client3.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+ 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+ 
+  let args = message.content.split(" ").slice(1);
+ 
+ 
+ 
+if (command == "تحدث") {
+let rank = message.guild.member(message.author).roles.find('TheBeatz', 'admin');
+if (!rank) return message.reply(' ')
+  message.channel.send(args.join("  "))
+    message.delete();
+  }
+});
+
+//client4
+
+
+client4.on('message', message => {
+    if(message.content === prefix+'راتب'){
+        message.channel.send('#daily')
+    }
+});
+ 
+client4.on('message', message => {
+    if(message.content === prefix+'مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+ 
+client4.on('message', message => {
+    if(message.content === prefix+'ريب'){
+        message.channel.send("#rep "+"<@" + myid + ">")
+    }
+});
+ 
+client4.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 9984511; x++) {
+        message.channel.send(`**NEW spam code by TheBeatz  - الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر ر الله اكبر الله اكبر **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+         
+        }
+      }
+});
+ 
+client4.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+ 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+ 
+  let args = message.content.split(" ").slice(1);
+ 
+ 
+ 
+if (command == "تحدث") {
+let rank = message.guild.member(message.author).roles.find('TheBeatz', 'admin');
+if (!rank) return message.reply(' ')
+  message.channel.send(args.join("  "))
+    message.delete();
+  }
+});
+
+//
+
+
+client5.on('message', message => {
+    if(message.content === prefix+'راتب'){
+        message.channel.send('#daily')
+    }
+});
+ 
+client5.on('message', message => {
+    if(message.content === prefix+'مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+ 
+client5.on('message', message => {
+    if(message.content === prefix+'ريب'){
+        message.channel.send("#rep "+"<@" + myid + ">")
+    }
+});
+ 
+client5.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 9984511; x++) {
+        message.channel.send(`**NEW spam code by TheBeatz  - الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر ر الله اكبر الله اكبر **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+         
+        }
+      }
+});
+ 
+client5.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+ 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+ 
+  let args = message.content.split(" ").slice(1); 
+ 
+if (command == "تحدث") {
+let rank = message.guild.member(message.author).roles.find('TheBeatz', 'admin');
+if (!rank) return message.reply(' ')
+  message.channel.send(args.join("  "))
+    message.delete();
+  }
+});
  
 client.login(process.env.TOKEN);
 client2.login(process.env.TOKEN2);
+client3.login(process.env.TOKEN3);
+client4.login(process.env.TOKEN4);
+client5.login(process.env.TOKEN5);
